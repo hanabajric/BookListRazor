9fb767ec154c4fb42eb62898a2a8cdbd92b8942e
hanabajric <hnbajric@gmail.com>
2021-10-11 22:26:50 +0200

    Add project files.

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] // kreirat id automatski 
+        public int Id { get; set; }
+
+        [Required] // ne moze bez imena
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
